f90d26112f36816053bb1ea9a2ab8f34011dfdc5
Rafal Krol <ameotoko1+github@gmail.com>
2026-05-23 12:00:37 +0200

    chore: start building out the first implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+/// Create a knowledge base from a given directory with a given provider.
+/// Defaults to this directory (`.`) and AWS.
+#[derive(Parser, Debug)]
+struct Cli {
+    /// Path to the directory to create the knowledge base from
+    dir: std::path::PathBuf,
+    /// Provider to use for the knowledge base, e.g. AWS< GCP, local
+    provider: String, // TODO: change to an enum
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    
+    println!("{:?}", args);
 }
