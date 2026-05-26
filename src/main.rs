3a648c22e4489c9dbeee242b9ceef1045f953653
Rafal Krol <ameotoko1+github@gmail.com>
2026-05-26 19:42:17 +0200

    feat: implement a mock command structure for starters

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,48 @@
-use clap::Parser;
+use std::fmt;
+use clap::{Parser, Subcommand, ValueEnum};
 
 /// Create a knowledge base from a given directory with a given provider.
 /// Defaults to this directory (`.`) and AWS.
 #[derive(Parser, Debug)]
+#[command(name = "mkb")]
+#[command(version, about = "Turn local folders into knowledge bases (KBs).", long_about = None)]
 struct Cli {
-    /// Path to the directory to create the knowledge base from
-    #[arg(short = 'd', long = "dir", default_value = ".")]
-    dir: std::path::PathBuf,
-    /// Provider to use for the knowledge base, e.g. AWS, GCP, local
-    #[arg(short = 'p', long = "provider", default_value = "aws")]
-    provider: String, // TODO: change to an enum
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// List all knowledge bases for a given provider
+    #[command(name = "ls", alias = "list")]
+    Ls {
+        #[arg(short = 'p', long = "provider", default_value_t = Provider::Aws)]
+        provider: Provider,
+    }
+}
+
+/// Provider to use for the knowledge base, e.g. AWS, GCP, local
+#[derive(ValueEnum, Clone, Debug)]
+enum Provider {
+    Aws,
+    // Gcp,
+    // Local,
+}
+
+impl fmt::Display for Provider {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Provider::Aws => write!(f, "aws"),
+            // Provider::Gcp   => write!(f, "gcp"),
+            // Provider::Local => write!(f, "local"),
+        }
+    }
 }
 
 fn main() {
-    let args = Cli::parse();
-    
-    println!("{:?}", args);
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Ls { provider } => println!("Listing knowledge bases for provider: {}", provider),
+    }
 }
